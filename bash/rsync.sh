adea77558ada73b336d7e95c2567ca3ef87c69e6
We Server <sfl@neusoft.edu.cn>
2012-07-19 00:06:11 +0800

    Use variable based directory.

--- bash/rsync.sh
+++ bash/rsync.sh
@@ -1,46 +1,47 @@
 #!/bin/bash
 
 LOCK=/tmp/rsyncing
+ROOT=/storage/mirror
 
 [ -f $LOCK ] && exit 0
 
 touch $LOCK
 
 # apache
-/usr/bin/rsync -azq --delete-delay rsync.apache.org::apache-dist /storage/mirror/apache/
+/usr/bin/rsync -azq --delete-delay rsync.apache.org::apache-dist $ROOT/apache/
 
 # centos
-/usr/bin/rsync -azq --delete-delay mirrors.kernel.org::centos /storage/mirror/centos/
+/usr/bin/rsync -azq --delete-delay mirrors.kernel.org::centos $ROOT/centos/
 
 # CPAN
-/usr/bin/rsync -azq --delete-delay mirrors.kernel.org::CPAN /storage/mirror/cpan/
+/usr/bin/rsync -azq --delete-delay mirrors.kernel.org::CPAN $ROOT/cpan/
 
 # cygwin
-/usr/bin/rsync -azq --delete-delay mirrors.kernel.org::sourceware/cygwin/ /storage/mirror/cygwin/
+/usr/bin/rsync -azq --delete-delay mirrors.kernel.org::sourceware/cygwin/ $ROOT/cygwin/
 
 # eclipse
-/usr/bin/rsync -azq --delete-delay download.eclipse.org::eclipseMirror /storage/mirror/eclipse/
+/usr/bin/rsync -azq --delete-delay download.eclipse.org::eclipseMirror $ROOT/eclipse/
 
 # epel
-/usr/bin/rsync -avq --delete-delay mirrors.kernel.org::fedora-epel /storage/mirror/epel/
+/usr/bin/rsync -avq --delete-delay mirrors.kernel.org::fedora-epel $ROOT/epel/
 [ $? -eq 0 ] && /usr/bin/report_mirror > /dev/null
 
 # gentoo
-/usr/bin/rsync -azq --delete-delay mirrors.kernel.org::gentoo /storage/mirror/gentoo/
+/usr/bin/rsync -azq --delete-delay mirrors.kernel.org::gentoo $ROOT/gentoo/
 
 # putty
-/usr/bin/rsync -azq --delete-delay rsync.chiark.greenend.org.uk::ftp/users/sgtatham/putty-website-mirror/ /storage/mirror/putty/
+/usr/bin/rsync -azq --delete-delay rsync.chiark.greenend.org.uk::ftp/users/sgtatham/putty-website-mirror/ $ROOT/putty/
 
 # pypi
-/usr/bin/pep381run -q /storage/mirror/pypi/
+/usr/bin/pep381run -q $ROOT/pypi/
 
 # ubuntu
-/usr/bin/rsync -azq --delete-delay mirrors.kernel.org::ubuntu /storage/mirror/ubuntu/
+/usr/bin/rsync -azq --delete-delay mirrors.kernel.org::ubuntu $ROOT/ubuntu/
 
 # ubuntu-release
-/usr/bin/rsync -azq --delete-delay mirrors.kernel.org::ubuntu-releases /storage/mirror/ubuntu-releases/
+/usr/bin/rsync -azq --delete-delay mirrors.kernel.org::ubuntu-releases $ROOT/ubuntu-releases/
 
 # timestamp
-date > /storage/mirror/timestamp.txt
+date > $ROOT/timestamp.txt
 
 rm -f $LOCK
