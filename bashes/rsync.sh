ffd8638736ce7a0f4752a7c4a5c46140cdd2a88c
Sun Fulong <sfl@neusoft.edu.cn>
2012-07-29 21:34:49 +0800

    镜像站同步脚本，抽取rsync公共方法

--- bashes/rsync.sh
+++ bashes/rsync.sh
@@ -7,69 +7,53 @@ ROOT=/storage/mirror
 
 touch $LOCK
 
+function rsync {
+   echo -1 > $ROOT/.$1.status
+   /usr/bin/rsync -azq --delete-delay $2 $ROOT/$1/
+   RESULT=$?
+   echo $RESULT > $ROOT/.$1.status
+   [$RESULT -eq 0] date "+%Y-%m-%d %H:%M:%S %Z" > $ROOT/.$1.timestamp
+}
+
 # apache
-echo -1 > $ROOT/.apache.status
-/usr/bin/rsync -azq --delete-delay rsync.apache.org::apache-dist $ROOT/apache/
-RESULT=$?
-echo $RESULT > $ROOT/.apache.status
-[ $RESULT -eq 0 ] date "+%Y-%m-%d %H:%M:%S %Z" >> $ROOT/.apache.timestamp
+rsync apache rsync.apache.org::apache-dist
 unset RESULT
 
 # centos
-echo -1 > $ROOT/.centos.status
-/usr/bin/rsync -azq --delete-delay mirrors.kernel.org::centos $ROOT/centos/
-RESULT=$?
-echo $RESULT > $ROOT/.centos.status
-[ $RESULT -eq 0 ] date "+%Y-%m-%d %H:%M:%S %Z" >> $ROOT/.centos.timestamp
+rsync centos mirrors.kernel.org::centos
 unset RESULT
 
-# CPAN
-echo -1 > $ROOT/.cpan.status
-/usr/bin/rsync -azq --delete-delay mirrors.kernel.org::CPAN $ROOT/cpan/
-RESULT=$?
-echo $RESULT > $ROOT/.cpan.status
-[ $RESULT -eq 0 ] date "+%Y-%m-%d %H:%M:%S %Z" >> $ROOT/.cpan.timestamp
+# cpan
+rsync cpan mirrors.kernel.org::CPAN
 unset RESULT
 
 # cygwin
-echo -1 > $ROOT/.cygwin.status
-/usr/bin/rsync -azq --delete-delay mirrors.kernel.org::sourceware/cygwin/ $ROOT/cygwin/
-RESULT=$?
-echo $RESULT > $ROOT/.cygwin.status
-[ $RESULT -eq 0 ] date "+%Y-%m-%d %H:%M:%S %Z" >> $ROOT/.cygwin.timestamp
+rsync cygwin mirrors.kernel.org::sourceware/cygwin/
 unset RESULT
 
 # eclipse
-echo -1 > $ROOT/.eclipse.status
-/usr/bin/rsync -azq --delete-delay download.eclipse.org::eclipseMirror $ROOT/eclipse/
-RESULT=$?
-echo $RESULT > $ROOT/.eclipse.status
-[ $RESULT -eq 0 ] date "+%Y-%m-%d %H:%M:%S %Z" >> $ROOT/.eclipse.timestamp
+rsync eclipse download.eclipse.org::eclipseMirror
 unset RESULT
 
 # epel
-echo -1 > $ROOT/.epel.status
-/usr/bin/rsync -avq --delete-delay mirrors.kernel.org::fedora-epel $ROOT/epel
-RESULT=$?
-echo $RESULT > $ROOT/.epel.status
-[ $RESULT -eq 0 ] date "+%Y-%m-%d %H:%M:%S %Z" >> $ROOT/.epel.timestamp
+rsync epel mirrors.kernel.org::fedora-epel
 [ $RESULT -eq 0 ] && /usr/bin/report_mirror > /dev/null
 unset RESULT
 
 # gentoo
-echo -1 > $ROOT/.gentoo.status
-/usr/bin/rsync -azq --delete-delay mirrors.kernel.org::gentoo $ROOT/gentoo/
-RESULT=$?
-echo $RESULT > $ROOT/.gentoo.status
-[ $RESULT -eq 0 ] date "+%Y-%m-%d %H:%M:%S %Z" >> $ROOT/.gentoo.timestamp
+rsync gentoo mirrors.kernel.org::gentoo
 unset RESULT
 
 # putty
-echo -1 > $ROOT/.putty.status
-/usr/bin/rsync -azq --delete-delay rsync.chiark.greenend.org.uk::ftp/users/sgtatham/putty-website-mirror/ $ROOT/putty/
-RESULT=$?
-echo $RESULT > $ROOT/.putty.status
-[ $RESULT -eq 0 ] date "+%Y-%m-%d %H:%M:%S %Z" >> $ROOT/.putty.timestamp
+rsync putty rsync.chiark.greenend.org.uk::ftp/users/sgtatham/putty-website-mirror/
+unset RESULT
+
+# ubuntu
+rsync ubuntu mirrors.kernel.org::ubuntu
+unset RESULT
+
+# ubuntu-release
+rsync ubuntu-release mirrors.kernel.org::ubuntu-releases
 unset RESULT
 
 # pypi
@@ -78,22 +62,6 @@ echo -1 > $ROOT/.pypi.status
 RESULT=$?
 echo $RESULT > $ROOT/.pypi.status
 [ $RESULT -eq 0 ] date "+%Y-%m-%d %H:%M:%S %Z" >> $ROOT/.pypi.timestamp
-unset RESULT
-
-# ubuntu
-echo -1 > $ROOT/.ubuntu.status
-/usr/bin/rsync -azq --delete-delay mirrors.kernel.org::ubuntu $ROOT/ubuntu/
-RESULT=$?
-echo $RESULT > $ROOT/.ubuntu.status
-[ $RESULT -eq 0 ] date "+%Y-%m-%d %H:%M:%S %Z" >> $ROOT/.ubuntu.timestamp
-unset RESULT
-
-# ubuntu-release
-echo -1 > $ROOT/.ubuntu-release.status
-/usr/bin/rsync -azq --delete-delay mirrors.kernel.org::ubuntu-releases $ROOT/ubuntu-releases/
-RESULT=$?
-echo $RESULT > $ROOT/.ubuntu-release.status
-[ $RESULT -eq 0 ] date "+%Y-%m-%d %H:%M:%S %Z" >> $ROOT/.ubuntu-release.timestamp
 unset RESULT
 
 rm -f $LOCK
