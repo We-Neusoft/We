c7ad500c5178bedc7a59cba1b724797f8748874c
Sun Fulong <sfl@neusoft.edu.cn>
2012-07-29 09:18:09 +0800

    修正rsync同步脚本
    有几个源的初始标识位被错误的标识成$?，已修正成-1

--- bashes/rsync.sh
+++ bashes/rsync.sh
@@ -23,42 +23,42 @@ echo -1 > $ROOT/.cpan
 echo $? > $ROOT/.cpan
 
 # cygwin
-echo $? > $ROOT/.cygwin
+echo -1 > $ROOT/.cygwin
 /usr/bin/rsync -azq --delete-delay mirrors.kernel.org::sourceware/cygwin/ $ROOT/cygwin/
 echo $? > $ROOT/.cygwin
 
 # eclipse
-echo $? > $ROOT/.eclipse
+echo -1 > $ROOT/.eclipse
 /usr/bin/rsync -azq --delete-delay download.eclipse.org::eclipseMirror $ROOT/eclipse/
 echo $? > $ROOT/.eclipse
 
 # epel
-echo $? > $ROOT/.epel
+echo -1 > $ROOT/.epel
 /usr/bin/rsync -avq --delete-delay mirrors.kernel.org::fedora-epel $ROOT/epel
 RESULT=$? && echo $RESULT > $ROOT/.epel && [ $RESULT -eq 0 ] && /usr/bin/report_mirror > /dev/null && unset RESULT
 
 # gentoo
-echo $? > $ROOT/.gentoo
+echo -1 > $ROOT/.gentoo
 /usr/bin/rsync -azq --delete-delay mirrors.kernel.org::gentoo $ROOT/gentoo/
 echo $? > $ROOT/.gentoo
 
 # putty
-echo $? > $ROOT/.putty
+echo -1 > $ROOT/.putty
 /usr/bin/rsync -azq --delete-delay rsync.chiark.greenend.org.uk::ftp/users/sgtatham/putty-website-mirror/ $ROOT/putty/
 echo $? > $ROOT/.putty
 
 # pypi
-echo $? > $ROOT/.pypi
+echo -1 > $ROOT/.pypi
 /usr/bin/pep381run -q $ROOT/pypi/
 echo $? > $ROOT/.pypi
 
 # ubuntu
-echo $? > $ROOT/.ubuntu
+echo -1 > $ROOT/.ubuntu
 /usr/bin/rsync -azq --delete-delay mirrors.kernel.org::ubuntu $ROOT/ubuntu/
 echo $? > $ROOT/.ubuntu
 
 # ubuntu-release
-echo $? > $ROOT/.ubuntu-release
+echo -1 > $ROOT/.ubuntu-release
 /usr/bin/rsync -azq --delete-delay mirrors.kernel.org::ubuntu-releases $ROOT/ubuntu-releases/
 echo $? > $ROOT/.ubuntu-release
 
