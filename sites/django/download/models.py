d65cec669131d0d91a6aa63d3ff87c48ab4c265c
Sun Fulong <sfl@neusoft.edu.cn>
2012-10-04 16:57:05 +0800

    数据库字段类型修改

--- sites/django/download/models.py
+++ sites/django/download/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Classification(models.Model):
-   name = models.CharField(max_length=None)
+   name = models.TextField()
    level = models.IntegerField(primary_key=True)
 
 class Software(models.Model):
-   name = models.CharField(max_length=None)
-   name_english = models.CharField(max_length=None)
-   name_pinyin_full = models.CharField(max_length=None, primary_key=True)
-   name_pinyin_short = models.CharField(max_length=None)
+   name = models.TextField()
+   name_english = models.TextField()
+   name_pinyin_full = models.TextField(primary_key=True)
+   name_pinyin_short = models.TextField()
    classification = models.ManyToManyField(Classification)
