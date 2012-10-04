1155c2575f72eed92402eca854daab6ad1520c43
Sun Fulong <sfl@neusoft.edu.cn>
2012-10-04 16:44:47 +0800

    数据库初始设计

--- sites/django/download/models.py
+++ sites/django/download/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Classification(models.Model):
+   name = models.CharField(max_length=None)
+   level = models.IntegerField(primary_key=True)
+
+class Software(models.Model):
+   name = models.CharField(max_length=None)
+   name_english = models.CharField(max_length=None)
+   name_pinyin_full = models.CharField(max_length=None, primary_key=True)
+   name_pinyin_short = models.CharField(max_length=None)
+   classification = models.ManyToManyField(Classification)
