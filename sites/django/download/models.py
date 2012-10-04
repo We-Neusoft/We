005d5a44a19aacbfd4c07e116144cdde4551ade3
Sun Fulong <sfl@neusoft.edu.cn>
2012-10-04 17:10:32 +0800

    数据库表添加默认值

--- sites/django/download/models.py
+++ sites/django/download/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Classification(models.Model):
    name = models.TextField()
    level = models.IntegerField(primary_key=True)
-   def __unicode__(self)
+   def __unicode__(self):
       return self.name
 
 class Software(models.Model):
@@ -12,5 +12,5 @@ class Software(models.Model):
    name_pinyin_full = models.TextField(primary_key=True)
    name_pinyin_short = models.TextField()
    classification = models.ManyToManyField(Classification)
-   def __unicode__(self)
+   def __unicode__(self):
       return self.name
