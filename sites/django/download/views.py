bb6ee9b066876264da926a71c1d95db5d8deaf85
Sun Fulong <sfl@neusoft.edu.cn>
2012-10-04 19:15:04 +0800

    下载站首页

--- sites/django/download/views.py
+++ sites/django/download/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+#coding=utf-8
+from download.models import Classification, Software
+from django.shortcuts import render_to_response
+
+def index(request):
+   classifications = Classification.objects.all()
+   softwares = Software.objects.all()
+   return render_to_response('download/index.weml', {'classifications': classifications, 'softwares': softwares})
