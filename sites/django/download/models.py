c89fdd33280c54494790b912f6785d1052a46e50
Sun Fulong <sfl@neusoft.edu.cn>
2012-10-04 17:27:29 +0800

    数据库表字段调整

--- sites/django/download/models.py
+++ sites/django/download/models.py
@@ -2,14 +2,16 @@ from django.db import models
 
 class Classification(models.Model):
    name = models.TextField()
-   level = models.IntegerField(primary_key=True)
+   name_english = models.TextField(primary_key=True)
+   name_pinyin_full = models.TextField()
+   name_pinyin_short = models.TextField()
    def __unicode__(self):
       return self.name
 
 class Software(models.Model):
    name = models.TextField()
-   name_english = models.TextField()
-   name_pinyin_full = models.TextField(primary_key=True)
+   name_english = models.TextField(primary_key=True)
+   name_pinyin_full = models.TextField()
    name_pinyin_short = models.TextField()
    classification = models.ManyToManyField(Classification)
    def __unicode__(self):
