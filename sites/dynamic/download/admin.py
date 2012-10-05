b846e4583d2123fe4b8c0e6f434ea410e2abda88
We Server <sfl@neusoft.edu.cn>
2012-10-05 14:56:26 +0800

    Active admin app

---
+++ sites/dynamic/download/admin.py
@@ -0,0 +1,5 @@
+from download.models import Classification, Software
+from django.contrib import admin
+
+admin.site.register(Classification)
+admin.site.register(Software)
--- sites/dynamic/download/models.py
+++ sites/dynamic/download/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 
 class Classification(models.Model):
-   name = models.TextField()
-   name_english = models.TextField(primary_key=True)
-   name_pinyin_full = models.TextField()
-   name_pinyin_short = models.TextField()
+   name = models.CharField(max_length=255)
+   name_english = models.CharField(max_length=255, primary_key=True)
+   name_pinyin_full = models.CharField(max_length=255)
+   name_pinyin_short = models.CharField(max_length=255)
    def __unicode__(self):
       return self.name
 
 class Software(models.Model):
-   name = models.TextField()
-   name_english = models.TextField(primary_key=True)
-   name_pinyin_full = models.TextField()
-   name_pinyin_short = models.TextField()
+   name = models.CharField(max_length=255)
+   name_english = models.CharField(max_length=255, primary_key=True)
+   name_pinyin_full = models.CharField(max_length=255)
+   name_pinyin_short = models.CharField(max_length=255)
    classification = models.ManyToManyField(Classification)
    def __unicode__(self):
       return self.name
