20d4b845556e0c685b780d352a05576051b8dce5
Sun Fulong <sfl@neusoft.edu.cn>
2012-10-05 17:58:03 +0800

    获得分类唯一名

--- sites/dynamic/download/views.py
+++ sites/dynamic/download/views.py
@@ -2,7 +2,15 @@
 from download.models import Classification, Software
 from django.shortcuts import render_to_response
 
+def key(string):
+   string = string.replace(' ', '-')
+   return string.lower()
+
 def index(request):
    classifications = Classification.objects.all()
+   for item in classifications:
+      classifications.key = key(classifications.name_english)
+
    softwares = Software.objects.all()
+
    return render_to_response('download/index.weml', {'classifications': classifications, 'softwares': softwares})
