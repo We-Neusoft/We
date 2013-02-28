bcad62fe7a5cac51209648ea520e06a3d3596aae
Sun Fulong <sfl@neusoft.edu.cn>
2013-02-28 23:31:56 +0800

    安致镜像同步脚本

---
+++ bashes/android-mirror.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+from os import path, stat, utime
+from urllib import urlretrieve, urlopen
+from time import mktime, strptime
+from xml.etree import ElementTree as ET
+
+def download(file_name, last_modified):
+   urlretrieve(base_url + file_name, work_dir + file_name)
+   utime(file_name, (last_modified, last_modified))
+
+   process(file_name)
+
+def process(file_name):
+   handle = urlopen(base_url + file_name)
+   headers = handle.info()
+   content_length = int(headers.getheader('Content-Length'))
+   last_modified = mktime(strptime(headers.getheader('Last-Modified'), '%a, %d %b %Y %H:%M:%S %Z'))
+
+   if path.exists(work_dir + file_name):
+      file_stat = stat(file_name)
+      if (file_stat.st_mtime == last_modified) and (file_stat.st_size == content_length):
+         return
+
+   download(file_name, last_modified)
+
+work_dir = '/storage/mirror/android/repository/'
+base_url = 'https://dl-ssl.google.com/android/repository/'
+xml_file = 'repository-7.xml'
+namespace = '{http://schemas.android.com/sdk/android/repository/7}'
+
+process(xml_file)
+
+repository = ET.parse(xml_file).getroot()
+for url in repository.iter(namespace + 'url'):
+   process(url.text)
